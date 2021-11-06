44ab5b70bf0892b28600af321259fea0a7eafbb8
Jojo Bautista <gjojob@yahoo.com>
2021-11-06 14:49:40 -0500

    updating countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,9 +26,9 @@ var questionItems = [
     }
 ];
 
+// var timeLeft = 0;
 
-
-$(".btn").one("click", function(event) {
+$(".btn").on("click", function(event) {
     event.preventDefault();
     
     $("#page1").attr("style", "display: initial !important");
@@ -36,11 +36,12 @@ $(".btn").one("click", function(event) {
     console.log("You click the button");
     
     $("#page1").attr("style", "display: none !important");
-    $("#timer").text("Time: 75 seconds"); 
+    $("#timeLeft").text("Time: 75 seconds"); 
     
     countdown(1);
     delay();
 
+    
     buildQuiz(0);    
 
     
@@ -59,6 +60,39 @@ function countdown(counter) {
         }
         counter--;
 }, 1000);
+
+};
+
+var timeLeft = 0;
+function setTimer(counter) {
+    counter = parseInt(counter);
+    if (counter !== -999) {
+        timeLeft = counter;
+    }
+
+    var interval = setInterval(function() {
+        if (counter === -999){
+            timeLeft -= 15;
+            clearInterval(interval);
+            setTimer(timeLeft);
+        }
+
+        if (timeLeft > 1) {
+            $("#timer").text("Time: " + timeLeft + " seconds");
+        }
+        else {
+            if (timeLeft < 0) {
+                timeLeft = 0;
+            }
+            $("#timer").text("Time: " + timeLeft + " second");
+        }
+        if (timeLeft === 0){
+            // run out of time
+            $("#count").text("You run out of time!");
+            clearInterval(interval);
+        }
+        timeLeft--;
+    }, 1000);
 };
 
 function delay() {
@@ -69,7 +103,14 @@ function delay() {
 
 
 function buildQuiz(i) {
-  
+    if (i === 0) {
+        setTimer(75);
+    }
+
+    if (i === questionItems.length) {
+        // add code for HS
+        return;
+    }
     console.log(i);
     $("#question").text(questionItems[i].question);
     $("#opt1").text(questionItems[i].options[0]);
@@ -77,18 +118,19 @@ function buildQuiz(i) {
     $("#opt3").text(questionItems[i].options[2]);
     $("#opt4").text(questionItems[i].options[3]);
     
-    $(".options").one("click", function(event) {
+    $(".options").off("click").one("click", function(event) {
         event.preventDefault();
         console.log($(event.currentTarget).text());
         if ($(event.currentTarget).text() === questionItems[i].answer) {
             console.log ("Correct");
         } else {
             console.log ("Wrong");
+            setTimer(-999);
         } 
         
         if ( i < questionItems.length){
             i++;
-            return buildQuiz(i);
+            buildQuiz(i);
         }
         
 
